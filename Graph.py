b293ad6e1b20a447cbbb8fec84876ca1e7dea4b1
George Harris <George.Harris@focusrite.lan>
2020-01-07 16:12:58 +0000

    removed ordered dict

--- Graph.py
+++ Graph.py
@@ -1,11 +1,9 @@
-from collections import OrderedDict
-
 class Node:
     def __init__(self, track_info, distances=None):
         self.track_info = track_info
 
         if distances is None:
-            distances = OrderedDict()
+            distances = {}
         self.distances = distances
     
     def __str__(self):
@@ -28,14 +26,19 @@ class Node:
         return self.track_info['duration_ms']
     
     def listened(self, previous):
-        self.distances[previous.track_id] += 1
-        previous.distances[self.track_id] = self.distances[previous.track_id]
-
+        print(self)
+        print(str(self.distances))
+        print(previous)
+        print(str(previous.distances))
+        print()
+        dist = self.distances[previous.track_id]
+        self.distances[previous.track_id] = dist + 1
+        previous.distances[self.track_id] = dist + 1
 
 class Graph:
     def __init__(self, nodes=None):
         if nodes is None:
-            nodes = OrderedDict()
+            nodes = {}
         self.nodes = nodes
     
     def __repr__(self):
@@ -49,19 +52,23 @@ class Graph:
         
         return string
     
+    def print_edges(self):
+        for node in self.nodes.values():
+            print(str(node1) + '-'*100)
+            for edge in node.distances.keys():
+                print(edge)
+    
     def health_check(self):
-        flag = True
-        print('Running graph health check...')
+
         for node1 in self.nodes.values():
             for node2 in self.nodes.values():
                 if node1.distances[node2.track_id] != node2.distances[node1.track_id]:
                     print(f'distances between {node1} and {node2} are asymmetrical. Please check this.')
-                    flag = False
+
                 if node2.track_id not in node1.distances:
                     print(f'node {node1} was missing distance to {node2}. Now fixed.')
                     node1.distances[node2.track_id] = 0
-                    flag = False
-        if Flag: print('Healthy!')
+
  
     def add_node(self, new_node):
         if new_node.track_id not in self.nodes:
