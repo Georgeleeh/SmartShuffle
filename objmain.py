5d16773f573045ff470936de676f1d2db842ebe4
George Harris <George.Harris@focusrite.lan>
2020-01-08 17:19:52 +0000

    now uses the Graph class

--- objmain.py
+++ objmain.py
@@ -1,4 +1,4 @@
-from Graph import Node
+from Graph import Graph, Node
 import time
 
 class Engine:
@@ -7,11 +7,10 @@ class Engine:
         self.config.read('config.cfg')
 
         if graph is None:
-            graph = {}
+            graph = Graph()
         self.graph = graph
 
         self.spotify = spotipy.Spotify(auth=self.token)
-
 
     @property
     def token(self):
@@ -21,92 +20,56 @@ class Engine:
         scope = 'user-read-currently-playing user-read-playback-state user-modify-playback-state'
         redirect_uri = 'http://localhost:8888/callback/'
         return util.prompt_for_user_token(username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
-
+    
     def learn(self):
-        def listen():
+        pb = self.spotify.current_playback()
 
-            playback = self.spotify.current_playback()
+        if pb is None:
+            print('no playback found')
+            return
 
-            if playback is None:
-                print('no playback found')
-                return
-            
-            current_node = Node(playback['item'])
-            print(current_node)
-            last_listened_node = current_node
+        if pb['item']['id'] not in graph.nodes:
+            current_node = Node(pb['item'])
+            graph.add_node(current_node)
+        else:
+            current_node = graph.nodes[pb['item']['id']]
 
-            self.add_node(current_node)
+        print(current_node)
 
-            wait_secs = 1
-            listening_duration_millis = 0
-            while True:
-                # is this necessary?
-                try:
-                    playback = self.spotify.current_playback()
-                except spotipy.client.SpotifyException:
-                    self.spotify = spotipy.Spotify(auth=self.token)
-                    playback = sp.current_playback()
-                
-                if playback is not None:
-                    # if you're listening to a new song since the last check
-                    if playback['item']['id'] != current_node.track_id:
+        last_listened_node = None
 
-                        if listening_duration_millis < current_node.track_duration_millis / 2:
-                            print('skipped early')
-                        else:
-                            # if this isn't the first song in the chain
-                            if last_listened_node != current_node:
-                                print('listened to a lot')
-                                self.increment_distance(last_listened_node, current_node)
+        DELAY_SECS = 1
 
-                                last_listened_node = current_node
-                    
-                        new_node = Node(playback['item'])
+        listening_duration_millis = 0
+        while True:
+
+            # might need to refresh token here, trying without
+            self.spotify.current_playback()
+
+            if pb is not None:
+                if pb['item']['id'] != current_node.track_id:
+
+                    if listening_duration_millis < current_node.track_duration_millis / 2:
+                        print('skipped early')
+                    else:
+                        if last_listened_node != None:
+                            print('listened to a lot')
+                            current_node.listened(last_listened_node)
+                        self.graph.health_check()
+                        last_listened_node = current_node
+
+                    if pb['item']['id'] not in self.graph.nodes:
+                        new_node = Node(pb['item'])
                         print(new_node)
-                        self.add_node(new_node)
+                        self.graph.add_node(new_node)
+                    else:
+                        new_node = self.graph.nodes[pb['item']['id']]
 
-                        current_node = new_node
-
-                listening_duration_millis = playback['progress_ms']
-
-                time.sleep(wait_secs)
-        
-        try:
-            listen()
-        except KeyboardInterrupt:
-            print('Interrupted')
-        
-
-    
-    def add_node(self, new_node):
-        if new_node.track_id not in self.graph.keys():
-            # connect new node to all nodes in graph
-            for node in self.graph.values():
-                new_node.distances[node.track_id] = 0
-
-            # add the new node to the graph
-            self.graph[new_node.track_id] = new_node
-
-            # connect every node in the graph to the new node
-            for key in self.graph.keys():
-                node = self.graph[key]
-                node.distances[new_node.track_id] = 0
-                self.graph[key] = node
-            
-            print('added node')
-    
-    def increment_distance(self, node1, node2):
-        print(f'updating distances in nodes {node1} and {node2}')
-        print(f'starting distances are {node1.distances.get(node2.track_id)} and {node2.distances.get(node1.track_id)}')
-        dist = node1.distances.get(node2.track_id)
-        self.graph[node1.track_id].distances[node2.track_id] = dist + 1
-        self.graph[node2.track_id].distances[node1.track_id] = dist + 1
-        self.print_graph()
-    
-    def print_graph(self):
-
-        for ynode in self.graph.values():
-            print(str(ynode.track_name[:5]), '\t\t', ' '.join(str(ynode.distances.get(xnode.track_id)) for xnode in self.graph.values()), '\t', ynode.track_id, '\n')
+                    current_node = new_node
+                    listening_duration_millis = 0
+                    
+                time.sleep(DELAY_SECS)
+                listening_duration_millis = pb['progress_ms']
 
     
     
