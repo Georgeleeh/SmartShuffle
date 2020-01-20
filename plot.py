a6fed562d554252effea6e81539d67b85b66031c
George Harris <George.Harris@focusrite.lan>
2020-01-20 15:42:08 +0000

    implement coloured edges

--- plot.py
+++ plot.py
@@ -1,9 +1,8 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-import pylab
+import pylab, colorsys, pickle
 
-import pickle
 from Graph import Graph, Node
 
 def open_graph(filename):
@@ -17,6 +16,9 @@ def open_graph(filename):
 
     return graph
 
+def weight_to_colour(weight):
+    return 'g' if weight == 0 else 'r' if weight == 1 else 'b'
+
 
 def mine_to_template(my_graph, plot_graph):
     def id_to_name(track_id):
@@ -25,7 +27,7 @@ def mine_to_template(my_graph, plot_graph):
 
     for node in my_graph.nodes.values():
         for edge in node.self_to_other.keys():
-            G.add_edges_from([(id_to_name(node.track_id), id_to_name(edge))], weight=node.self_to_other[edge])
+            G.add_edges_from([(id_to_name(node.track_id), id_to_name(edge))], weight=node.self_to_other[edge], color=weight_to_colour(node.self_to_other[edge]))
 
 
 G = nx.DiGraph(directed=True)
@@ -36,14 +38,20 @@ graph = open_graph(FILENAME)
 
 mine_to_template(graph, G)
 
+edges = G.edges()
+edge_labels = dict([((u,v,),d['weight']) for u,v,d in G.edges(data=True)])
+edge_colors = [G[u][v]['color'] for u,v in edges]
 
-edge_labels=dict([((u,v,),d['weight']) for u,v,d in G.edges(data=True)])
-node_labels = {node:node for node in G.nodes()}
+nodes = G.nodes()
+node_labels = {node:node[:4] for node in G.nodes()}
 
-pos=nx.circular_layout(G)
+
+
+
+pos=nx.spring_layout(G)
 
 
 nx.draw_networkx_labels(G, pos, labels=node_labels)
-nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels,labels=[node for node in G])
-nx.draw(G,pos, node_size=500)
+#nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels,labels=[node for node in G])
+nx.draw(G,pos, edge_color=edge_colors, node_size=200)
 pylab.show()
