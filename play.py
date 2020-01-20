a1d42489ad03bbab5580f07125c631074e45b08c
George Harris <George.Harris@focusrite.lan>
2020-01-20 15:42:50 +0000

    add shuffle and basic learning modes

--- play.py
+++ play.py
@@ -4,9 +4,7 @@ import configparser
 import spotipy.oauth2 as oauth2
 import spotipy.util as util
 
-import pickle
-import sys, os
-
+import pickle, sys, os, random
 from Graph import Graph, Node
 
 def main(graph): 
@@ -15,7 +13,7 @@ def main(graph):
     client_id = config.get('SPOTIFY', 'CLIENT_ID')
     client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
     username = config.get('SPOTIFY', 'USERNAME')
-    scope = 'user-read-currently-playing user-read-playback-state streaming'
+    scope = 'user-read-currently-playing user-read-playback-state user-modify-playback-state'
     redirect_uri = 'http://localhost:8888/callback/'
     token = util.prompt_for_user_token(username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
 
@@ -24,7 +22,37 @@ def main(graph):
     nodes = {}
     sp = spotipy.Spotify(auth=token)
 
-    sp.start_playback(uris=['spotify:track:7lEptt4wbM0yJTvSG5EBof'])
+    shuffled = [node.track_info['uri'] for node in graph.nodes.values()]
+
+    playlist = learn_mode(graph)
+
+    sp.start_playback(uris=playlist)
+
+# dumb shuffles all known songs
+def shuffle_all(graph):
+    shuff = [node.track_info['uri'] for node in graph.nodes.values()]
+    random.shuffle(shuff)
+    return shuff
+
+def learn_mode(graph, n=10):
+    playlist = []
+    nodes = list(graph.nodes.values())
+    random.shuffle(nodes)
+    
+    for i in range(n):
+        for node in nodes:
+            if len(playlist) == 0:
+                playlist.append(node)
+                break
+            else:
+                if node.self_to_other.get(playlist[-1].track_id) is None and node not in playlist:
+                    playlist.append(node)
+                    break
+
+    playlist = [node.track_info['uri'] for node in playlist]
+    return playlist
+
+
 
 
 
@@ -33,19 +61,14 @@ def main(graph):
 
 if __name__ == '__main__':
 
-    pickle_file = open('pickle.pkl', 'rb')
+    pickle_file = open('mypickle(skips).pkl', 'rb')
     graph = pickle.load(pickle_file)
-    graph.health_check()
     pickle_file.close()
 
     try:
         main(graph)
     except KeyboardInterrupt:
         print('Interrupted')
-        afile = open('pickle.pkl', 'wb')
-        pickle.dump(graph, afile)
-        afile.close()
-        print('saved pickle.pkl')
         try:
             sys.exit(0)
         except SystemExit:
